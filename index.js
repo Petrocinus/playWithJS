4dcbce4268a09d5c009fea3a17c5a09b633ab697
Petrocinus <104753315+Petrocinus@users.noreply.github.com>
2022-12-18 22:29:36 +0200

    Add files

---
+++ index.js
@@ -0,0 +1,20 @@
+// random of first image
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+var randomImageSource1 = "images/" + randomDiceImage1;
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource1);
+
+// random of second image
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+document.querySelectorAll("img")[1].setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerText = "🎈PLayer 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerText = "Player 2 Wins!🎈";
+} else {
+    document.querySelector("h1").innerText = "Draw!";
+}
+
